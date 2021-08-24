31319e9139fffc64dd97269a5523927450935ba7
W.Worker <wwit.llt@gmail.com>
2021-08-24 18:40:59 +0900

    Complete Token Contract

--- truffle-config.js
+++ truffle-config.js
@@ -44,7 +44,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.0",
+      version: "0.7.6",
       settings: {
         optimizer: {
           enabled: true,
